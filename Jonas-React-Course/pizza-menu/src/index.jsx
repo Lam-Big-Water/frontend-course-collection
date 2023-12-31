926e802a96d09cdb81ed52386e73fa7e5b7d971e
Sam <xlin67948@gmail.com>
2023-12-31 20:43:52 +0800

    pizzaMenu is done

--- Jonas-React-Course/pizza-menu/src/index.jsx
+++ Jonas-React-Course/pizza-menu/src/index.jsx
@@ -89,14 +89,13 @@ function Menu() {
 }
 
 function Pizza({pizzaObj}) {
-  if (pizzaObj.soldOut) return null;
   return (
-      <li className='pizza'>
+      <li className = {pizzaObj.soldOut ? 'pizza sold-out' : `pizza`}>
           <img src={pizzaObj.photoName} alt={pizzaObj.name} />
           <div>
             <h3>{pizzaObj.name}</h3>
             <p>{pizzaObj.ingredients}</p>
-            <span>{pizzaObj.price}</span>
+            <span>{pizzaObj.soldOut ? 'SOLD-OUT' : pizzaObj.price}</span>
           </div>
       </li>
   )
