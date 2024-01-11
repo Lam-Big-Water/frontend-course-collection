d2e9adf1b66cb64cfda0fcc71f3646f34f406b42
Sam <xlin67948@gmail.com>
2024-01-11 21:00:46 +0800

    Closure Part it's done

--- JS-TAC/04-The-2Pillars/Function/Closures/script.js
+++ JS-TAC/04-The-2Pillars/Function/Closures/script.js
@@ -118,3 +118,11 @@ startOnce();
 startOnce();
 startOnce();
 
+const array = [1,2,3,4];
+for (var i=0; i < array.length; i++) {
+    (function(closureI) {
+        setTimeout(function() {
+            console.log('I am at index' + array[closureI])
+        }, 3000)
+    })(i)
+}
