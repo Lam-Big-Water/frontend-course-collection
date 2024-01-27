2a329373e170ec30882fc256fd1dea3184cf3a4f
Sam <xlin67948@gmail.com>
2024-01-27 19:52:49 +0800

    P129

---
+++ JS-TAC/07-Modules/Module-Pattern/script.js
@@ -0,0 +1,23 @@
+// Global Scope
+    // Module Scope
+        // Function Scope
+            // Block Scope - let and const 
+
+
+// IIFE
+// Module Pattern
+var fightModule = (function() {
+    var harry = 'potter';
+    var voldemort ='He who must not be named';
+
+    function fight(char1, char2) {
+        var attack1 = Math.floor(Math.random() * char1.length);
+        var attack2 = Math.floor(Math.random() * char2.length);
+        return attack1 > attack2 ? `${char1} wins` : `${char2} wins`
+    }
+    return {
+        fight: fight
+    }
+})()
+// Other pieces of code can now use this functionality.
+fightModule.fight('SamLam', 'Jane')
