3b925b0333b316a32a3d55ff760659e1a4ae17b6
Sam <xlin67948@gmail.com>
2024-01-31 20:59:00 +0800

    Array it's done

--- JS-TAC/09-Data-Structures/Array/script.js
+++ JS-TAC/09-Data-Structures/Array/script.js
@@ -98,3 +98,46 @@ const data = 'this is important';
 const reverse3 = str => [...str].reverse().join('');
 console.log(reverse3(data));
 console.log(data);
+
+function mergeSortedArrays(array1, array2) {
+    const mergedArray = [];
+    let array1Item = array1[0];
+    let array2Item = array2[0];
+    let i = 1;
+    let j = 1;
+
+    // Check input
+    if (array1.length === 0) {
+        return array2
+    }
+
+    if (array2.length === 0) {
+        return array1
+    }
+
+    while (array1Item || array2Item) {
+        console.log(array1Item, array2Item)
+        if (!array2Item || array1Item < array2Item) {
+            mergedArray.push(array1Item);
+            array1Item = array1[i];
+            i++;
+        } else {
+            mergedArray.push(array2Item);
+            array2Item = array2[j];
+            j++;
+        }
+    }
+
+    return mergedArray;
+}
+
+console.log(mergeSortedArrays([1,2,3], [4,5,6]));
+
+console.log(1 || 1 || false)
+// function mergeSortedArrays(arr1, arr2) {
+//     const collect = [...arr1, ...arr2];
+//     collect.sort((a,b) => a - b);
+//     return collect;
+// }
+
+// console.log(mergeSortedArrays([1,2,3], [6,5,4]))
