a27ffaa8609c4008c626d4a1569ccc99c714228c
Sam <xlin67948@gmail.com>
2024-02-05 19:56:44 +0800

    P188

--- JS-TAC/09-Data-Structures/Hash/script.js
+++ JS-TAC/09-Data-Structures/Hash/script.js
@@ -9,4 +9,61 @@ let user = {
 
 user.age // O(1)
 user.spell = 'Love Song'; // O(1)
-user.scream(); // O(1)
+user.scream(); // O(1) 
+
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for (let i=0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+            console.log(hash);
+        }
+        return hash;
+    }
+
+    set(key, value) {
+        let address = this._hash(key);
+        if (!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value]);
+        return this.data;
+        
+    } //O(1)
+
+    get(key) {
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        console.log(currentBucket);
+        if (currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1]
+                }
+            }
+        } //O(1)
+        return undefined
+    }
+
+    keys() {
+        const keysArray = [];
+        for (let i = 0; i < this.data.length; i++) {
+            if(this.data[i]) {
+                keysArray.push(this.data[i][0][0])
+            }
+        }
+        return keysArray;
+    }
+}
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+myHashTable.set('apple', 99);
+myHashTable.set('orange', 98);
+
+console.log(myHashTable.get('grapes'), 
+myHashTable.keys()
+)
