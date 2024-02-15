e46f49c9402f8cb5041f32a2e3b498005a7169e4
Sam <xlin67948@gmail.com>
2024-02-15 21:37:16 +0800

    P163

--- JS-TAC/09-Data-Structures/Hash/script.js
+++ JS-TAC/09-Data-Structures/Hash/script.js
@@ -67,3 +67,28 @@ myHashTable.set('orange', 98);
 console.log(myHashTable.get('grapes'), 
 myHashTable.keys()
 )
+
+// this function it can't return the correct answer. 
+function firstRecurringCharacter(input) {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[i] === input[j]) return input[i]
+        }
+    }
+    return undefined
+} // O(n^2)
+console.log('result ' + firstRecurringCharacter([2,1,1,2,3,3,5]));
+
+function advance(input) {
+    let map = {};
+    for (let i = 0; i < input.length; i++) {
+        if (map[input[i]] !== undefined) {
+            return input[i]
+        } else {
+            map[input[i]] = i
+        }
+        console.log(map)
+    }
+    return undefined
+}
+console.log('result ' + advance([2,1,1,2,3,3,5]));
